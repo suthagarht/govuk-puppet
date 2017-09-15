13e8060eb687b1b72c28d5559d9735ee9f839d83
Isabell Long <isabell@issyl0.co.uk>
2017-09-15 17:09:03 +0100

    [WIP] Specify the correct db hostname for Transition in AWS

--- modules/govuk/manifests/apps/transition/postgresql_db.pp
+++ modules/govuk/manifests/apps/transition/postgresql_db.pp
@@ -12,8 +12,10 @@ class govuk::apps::transition::postgresql_db (
   $password = '',
   $backend_ip_range = '10.3.0.0/16',
   $rds = false,
+  $db_hostname = undef,
 ) {
   govuk_postgresql::db { 'transition_production':
+    db_hostname             => $db_hostname,
     user                    => 'transition',
     password                => $password,
     allow_auth_from_backend => true,
