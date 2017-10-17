d80f8a0c480d420282c82b52aaf70c815b08c576
Laura Martin <surminus@gmail.com>
2017-10-17 10:17:24 +0100

    Add Jenkins job for Swarm apps

---
+++ modules/govuk_jenkins/manifests/job/deploy_swarm_app.pp
@@ -0,0 +1,31 @@
+# == Class: govuk_jenkins::job::deploy_swarm_app
+#
+# Create a file on disk that can be parsed by jenkins-job-builder
+#
+# === Parameters
+#
+# [*app_domain*]
+#   The hostname where applications are served from
+#
+# [*auth_token*]
+#   Token which will allow this job to be triggered remotely
+#
+# [*applications*]
+#   A hash of applications which contains the application name and
+#   port that is passed to the deployment script.
+#
+class govuk_jenkins::job::deploy_swarm_app (
+  $app_domain = undef,
+  $auth_token = undef,
+  $applications = undef,
+) {
+  if $::aws_migration {
+    $aws_deploy = true
+  }
+
+  file { '/etc/jenkins_jobs/jobs/deploy_swarm_app.yaml':
+    ensure  => present,
+    content => template('govuk_jenkins/jobs/deploy_swarm_app.yaml.erb'),
+    notify  => Exec['jenkins_jobs_update'],
+  }
+}
